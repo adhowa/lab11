59185395a00d101de17b8d490b57139b0b781357
adhowa <aubreyhowa@gmail.com>
2014-10-28 15:30:17 -0500

    made oval move faster

--- lab11-70pt.py
+++ lab11-70pt.py
@@ -22,14 +22,17 @@ def animate():
     # Get the x and y co-ordinates of the circle
     x1, y1, x2, y2 = drawpad.coords(circle)
     if x2 > drawpad.winfo_width(): 
-        direction = - 1
+        direction = - 5
     elif x1 < 0:
-        direction = 1
+        direction = 5
     #Move our oval object by the value of direction
     drawpad.move(circle,direction,0)
     # Wait for 1 millisecond, then recursively call our animate function
     drawpad.after(1, animate)
 
 # Kick off our animation
+
+
+
 animate()
 root.mainloop()
